ae986e29e7dc3793b7ab60c3a051b7fe721293a9
hp_computer <andrademareshp@gmail.com>
2023-01-09 23:29:46 -0600

    Metodos CRUD

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -12,21 +12,43 @@ public class WeatherForecastController : ControllerBase
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
+    private static List<WeatherForecast> ListWeatherForecasts = new List<WeatherForecast>();
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
         _logger = logger;
+        // ! negación  --> any()   -->tiene algun registro
+        if (ListWeatherForecasts == null || !ListWeatherForecasts.Any())
+        {
+            ListWeatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToList();
+        }
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return ListWeatherForecasts;
     }
+
+    [HttpPost]
+    public IActionResult Post(WeatherForecast weatherForecast)
+    {
+        ListWeatherForecasts.Add(weatherForecast);
+        return Ok();
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        // recibir el index de la posicion que deseamos eliminar 
+        ListWeatherForecasts.RemoveAt(index);
+        return Ok();
+    }
+
 }
